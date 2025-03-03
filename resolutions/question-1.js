40aa015a82f75eae2790d364743c90a733c0765b
Gabriel Morais <37519878+gabrielbriks@users.noreply.github.com>
2025-03-03 16:52:37 +0000

    Resolução da questão 1

---
+++ resolutions/question-1.js
@@ -0,0 +1,14 @@
+function main() {
+  let index = 13;
+  let sum = 0;
+  let K = 0;
+
+  while (K < index) {
+    K++;
+    sum += K;
+  }
+
+  console.log("Result: ", sum); //Result: 91
+}
+
+main();
